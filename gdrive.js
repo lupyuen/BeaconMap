b197071b7588f9ca025caee22167449b3519dbe8
lupyuen <lupyuen@gmail.com>
2016-08-01 03:00:43 +0800

    Testing downloads

--- gdrive.js
+++ gdrive.js
@@ -2,53 +2,99 @@
 
 const fs = require('fs');
 const readline = require('readline');
+const google = require('googleapis');
 const googleAuth = require('google-auth-wrapper');
+const googleDrive = google.drive('v3');
 const GDriveWrapper = require('google-drive-wrapper');
 
-function authorize(callback) {
-    googleAuth.authorize('../temp', 'SEED01-beaconmap1',
-        ['https://www.googleapis.com/auth/drive'],
-        (url, provideCode) => {
-            console.log('Please vist this url, authorize the app and return the code provided', url);
 
-            //provideCode(`4/usyGbYvpbVi6C-I6TQHFgIBDCib1rT4TXtAOWRIIUnc`, ()=>{}); ////
-
-            var read = readline.createInterface({
-                input: process.stdin,
-                output: process.stdout
-            });
-            read.question('What was the code:?', code => {
-                read.close();
-                provideCode(code, err => {
-                    if (err !== undefined) console.log('err:' + err);
-                    return callback(err);
-                });
-            });
-        });
+//  Lists the names and IDs of up to 10 files.
+//  @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+function listFiles(auth, google, callback) {
+    googleDrive.files.list({
+        auth: auth,
+        pageSize: 10,
+        fields: "nextPageToken, files(id, name)"
+    }, function(err, response) {
+        if (err) {
+            console.log('The API returned an error: ' + err);
+            return callback(err);
+        }
+        var files = response.files;
+        if (files.length == 0) {
+            console.log('No files found.');
+        } else {
+            console.log('Files:');
+            for (var i = 0; i < files.length; i++) {
+                var file = files[i];
+                console.log('%s (%s)', file.name, file.id);
+            }
+        }
+        return callback(null)
+    });
 }
 
-function execute(callback) {
-    googleAuth.execute('../temp', 'SEED01-beaconmap1', (auth, google) => {
-        var wrapper = new GDriveWrapper(auth, google);
-        const gdriveDirectory = '';
-        wrapper.listFiles(gdriveDirectory, err => {
-            //wrapper.downloadNewFiles('backups', './download', err => {
-            if (err) {
-                console.log(err);
-            }
+const key = require('../SEED01-service1.json');
+const scopes = ['https://www.googleapis.com/auth/drive.metadata.readonly'];
+const jwtClient = new google.auth.JWT(key.client_email, null, key.private_key, scopes, null);
+
+function listFiles(callback) {
+    jwtClient.authorize((err, tokens) => {
+        if (err) {
+            console.log(err);
             return callback(err);
+        }
+        // Make an authorized request to list Drive files.
+        googleDrive.files.list({ auth: jwtClient }, (err2, resp) => {
+            // handle err and response
+            if (err2) {
+                console.log(err2);
+                return callback(err2);
+            }
+            for (const file of resp.files) {
+                const filename = file.name;
+                if (filename.indexOf('Beacon Map') !== 0) continue;
+                return callback(null, file);
+            }
+            return callback(null);
         });
     });
 }
 
-/*
-authorize(err => {
-    if (err) process.exit(1);
-    process.exit(0);
-});
-*/
+function downloadFile(fileId, mimeType, dest_filename, callback) {
+    const dest = fs.createWriteStream(dest_filename);
+    googleDrive.files.export({fileId, mimeType})
+        .on('end', function() {
+            console.log('Done');
+            return callback(null);
+        })
+        .on('error', function(err) {
+            console.log('Error during download', err);
+            return callback(err);
+        })
+        .pipe(dest);
+}
 
-execute(err2 => {
-    if (err2) process.exit(2);
-    process.exit(0);
+//  List the files.
+//  Look for "Beacon Map..."
+//  Download as SVG.
+listFiles((err, file) => {
+    if (err) {
+        console.log(err);
+        process.exit(1);
+    }
+    const fileId = file.id;
+    const mimeType = 'image/svg+xml';
+    const dest_filename = 'DownloadedBeaconMap.svg';
+    downloadFile(fileId, mimeType, dest_filename, err2 => {
+        if (err2) {
+            console.log(err2);
+            process.exit(2);
+        }
+        console.log('Done');
+        process.exit(0);
+    });
 });
+
+
+
