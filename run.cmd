ee657152557966948a2121eb09bf49efdde7bc32
lupyuen <lupyuen@gmail.com>
2016-08-02 20:28:04 +0800

    Added pdf2json

--- run.cmd
+++ run.cmd
@@ -1,3 +1,5 @@
+node node_modules\pdf2json\pdf2json.js -t -c -m -f "Beacon Slides.pdf"
+
 node node_modules\svgson\bin\svgson.js --pretty --input "Sample Map.svg"  --output "Sample Map.json"
 
 ::node node_modules\nano-svg2js\bin\svg-to-js.js --stringify "Sample Map.svg"
