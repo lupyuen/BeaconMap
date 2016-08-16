974c10c74b2fa4163fdc103270cc2a9c67a0d627
lupyuen <lupyuen@gmail.com>
2016-08-16 13:23:36 +0800

    Dump text with coordinates

--- pdf2json.js
+++ pdf2json.js
@@ -1,7 +1,58 @@
 /*
  node pdf2json.js -t -c -m -f "Beacon Slides.pdf"
 
-Need to change BeaconMap\node_modules\pdf2json\lib\pdffield.js:
+ Change BeaconMap/node_modules/pdf2json/lib/pdf.js line 402:
+
+     cls.prototype.getRawTextContent = function() {
+        let retVal = "";
+        if (!this.needRawText)
+            return retVal;
+
+        _.each(this.rawTextContents, function(textContent, index) {
+            let prevText = null;
+            _.each(textContent.bidiTexts, function(textObj, idx) {
+	            if (prevText) {
+		            if (Math.abs(textObj.y - prevText.y) <= 9) {
+			            prevText.str += textObj.str;
+		            }
+		            else {
+			            retVal += (parseInt(prevText.x * 10) / 10) + ", " +
+                            (parseInt(prevText.y * 10) / 10) + ": " +
+                            prevText.str  + "\r\n"; ////
+			            prevText = textObj;
+		            }
+	            }
+	            else {
+		            prevText = textObj;
+	            }
+
+            });
+	        if (prevText) {
+		        retVal += prevText.str;
+	        }
+            retVal += "\r\n----------------Page (" + index + ") Break----------------\r\n";
+        });
+
+        return retVal;
+    };
+
+
+Need to change BeaconMap\node_modules\pdf2json\lib\pdffield.js line 76:
+
+Change from:
+
+cls.isFormElement = function(field) {
+    let retVal = false;
+    switch(field.subtype) {
+        case 'Widget': retVal = cls.isWidgetSupported(field); break;
+        default:
+            nodeUtil.p2jwarn("Unsupported: field.type of " + field.subtype);
+            break;
+    }
+    return retVal;
+};
+
+to:
 
  cls.isFormElement = function(field) {
      let retVal = false;
@@ -15,6 +66,12 @@ Need to change BeaconMap\node_modules\pdf2json\lib\pdffield.js:
              break;
          ////  Lup Yuen: End of changes.
          case 'Widget': retVal = cls.isWidgetSupported(field); break;
+        default:
+            nodeUtil.p2jwarn("Unsupported: field.type of " + field.subtype);
+            break;
+    }
+    return retVal;
+};
 
 
 */
@@ -35,4 +92,37 @@ cmd.complete = function(err) {
         process.exit(0);
     });
 };
+
+cmd.getRawTextContent = function() {
+    debugger;////
+    let retVal = "";
+    if (!this.needRawText)
+        return retVal;
+
+    _.each(this.rawTextContents, function(textContent, index) {
+        let prevText = null;
+        _.each(textContent.bidiTexts, function(textObj, idx) {
+            if (prevText) {
+                if (Math.abs(textObj.y - prevText.y) <= 9) {
+                    prevText.str += textObj.str;
+                }
+                else {
+                    retVal += prevText.str  + "\r\n";
+                    prevText = textObj;
+                }
+            }
+            else {
+                prevText = textObj;
+            }
+
+        });
+        if (prevText) {
+            retVal += prevText.str;
+        }
+        retVal += "\r\n----------------Page (" + index + ") Break----------------\r\n";
+    });
+
+    return retVal;
+};
+
 cmd.start();
